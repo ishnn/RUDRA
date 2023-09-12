25c94371fb2a625d449e9e2960a93e28aa107363
Aditya Sharma <99022811+adidevs@users.noreply.github.com>
2023-09-12 13:07:59 +0530

    setup

---
+++ client/src/components/TitleBar.jsx
@@ -0,0 +1,9 @@
+import '../index.css'
+
+export default function TitleBar() {
+    return (
+        <div className="title-bar">
+            <div className="text-xl font-medium">Rudra</div>
+        </div>
+    )
+    }
